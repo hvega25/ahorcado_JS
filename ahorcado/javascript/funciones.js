8cfee8fcd9ab1f5c1af6bae9192485c05bca148a
hvega25 <2023_Herson.Vega@iticbcn.cat>
2023-11-08 20:22:47 +0100

    Falta contador,estadisticas

--- ahorcado/javascript/funciones.js
+++ ahorcado/javascript/funciones.js
@@ -1,4 +1,4 @@
-
+init();
 //funcion principal que es la que decide el curso del juego
 function init() {
   var control_switch = false;
@@ -8,10 +8,12 @@ function init() {
   while (control_switch != true) {
     if (opcion >= 1 && opcion <= 3) {
       switch (opcion) {
-        case 1:
+        case 1: 
           jugar();
+          control_switch = true;
           break;
         case 2:
+          control_switch = true;
           break;
         case 3:
           control_switch = true;
@@ -24,7 +26,7 @@ function init() {
   }
 }
 
-//funcion para acortar codigo solo muestra un menu
+//Función para acortar código solo muestra un menu
 function menu() {
   console.log(`Opciones
     1. Iniciar el juego
@@ -33,43 +35,51 @@ function menu() {
     `);
 }
 
-//funcion para imprimir los simbolos
+//Función que despliega el juego
 function jugar() {
   var palabra = prompt(`Ingresa una palabra`);
   var control_jugar = false;
   var intentos = 0;
-  var aux = "";
-
+  var intentos_maximos = 8;
+  //variable que guarda la palabra transformada de string en un array para su facil calculo
+  var palabraL = conversion_palabra(palabra);
   console.log(`Que empiece el juego!!!`);
-
+  imprimir_guiones(`${palabraL[1]}`);
+  var letras_usadas = [];
   //desde aqui iniciar el codigo para jugar validando si tiene 1 sola letra y si es aprobada
   while (control_jugar != true) {
     var letra = prompt(`Ingresa una letra`);
-    //valida si es una letra y si existe en la palabra
 
-    if (
-      validores_letra(letra) == true &&
-      palabra.includes(letra) == true &&
-      aux.includes(" _ ") == true
-    ) {
-      for (var f = 0; f < aux.length; f++) {}
-    } else {
-      console.log("no es letra valida");
-    } //fin de comprobacion de si una letra valida
+    //aqui meto el contador
+    if (intentos < intentos_maximos + 1) {
+      //primer if que controla si la letra existe en la palabra a buscar
+      if (palabraL[0].includes(letra)) {
+        //if que validad si la letra es valida es decir si esta dentro de a y z en mayuscula y minuscula
+        if (validores_letra(letra) == true) {
+          var actualizada = actualizar_palabra(palabraL, letra);
+          imprimir_guiones(`${palabraL[1]}`);
+          console.log(`Letra falladas: ${letras_usadas}`);
+          //if que valida si existe incognitas en la palabtra
+          if (validar_guion(actualizada[1]) != true) {
+            control_jugar = true;
+          }
+        } else {
+          console.log(`Letra no valida`);
+          intentos = intentos  + 1 ;
+        }
+      } else {
+        letras_usadas.push(letra);
+      }
+    }else{
+      console.log(` HAS PERDIDO !  !  !`);
+      control_jugar = true;
+    }
+
+    //termina aqui contador
   }
 }
 
-//funcion para calcular los simolos
-function dibuja_palabra(palabra) {
-  var simbolos = "";
-  for (var g = 0; g < palabra.length; g++) {
-    simbolos = simbolos + " _ ";
-  }
-
-  return simbolos;
-}
-
-//valida si es una letra valida
+//valida si es una letra valida es decir si es una letra de la a a la z en mayuscula y miniscula
 function validores_letra(letra) {
   if (
     letra.length <= 1 &&
@@ -84,23 +94,63 @@ function validores_letra(letra) {
   }
 }
 
-function prueba(palabra, letra) {
+//Método que convierte la palabra de String a Arrat y a su vez otra palabra que es un incognita
+function conversion_palabra(palabra) {
+  var arrayPalabra = [];
+  var arrayEncriptado = [];
+  var retorno = [];
+
+  for (var a = 0; a < palabra.length; a++) {
+    arrayPalabra.push(palabra.charAt(a));
+    arrayEncriptado[a] = "_";
+  }
+
+  retorno.push(arrayPalabra);
+  retorno.push(arrayEncriptado);
+
+  return retorno;
+}
+
+//Función que actualiza la palabra colocando la letra en el guion si esta coincide con la palabra a buscar
+function actualizar_palabra(palabraL, letra) {
+  var longitud = palabraL[0].length;
+  var palabra_normal = palabraL[0];
+  var encriptada = palabraL[1];
+
+  for (var k = 0; k < longitud; k++) {
+    if (encriptada[k] == "_") {
+      if (palabra_normal[k] == letra) {
+        encriptada[k] = letra;
+      } else {
+        encriptada[k] = "_";
+      }
+    }
+  }
+  palabraL[1] = encriptada;
+
+  return palabraL;
+}
+
+//Método que valida si en la palabra existen letras sin acertar
+function validar_guion(p) {
+  for (var d = 0; d < p.length; d++) {
+    if (p[d] == "_") {
+      return true;
+    }
+  }
+  return false;
+}
+
+//Método que imprime los guiones de la palabra en consola para indicarle al usuario cuantas letras tiene que adivinar
+function imprimir_guiones(pal) {
   var simbolos = "";
-  var aux = palabra;
-  for (var a = 0; a < aux.length; a++) {
-    if(aux[a] == letra ){
-        simbolos = simbolos +" "+ letra;
-    }else{
-      simbolos += " _ ";
-    } 
+  for (var s = 0; s < pal.length; s++) {
+    if (pal[s] != "_") {
+      simbolos = simbolos + pal[s] + " ";
+    } else {
+      simbolos = simbolos + " _ ";
+    }
   }
 
   console.log(simbolos);
 }
-
-function cambio (){}
-
-
-var palabra = "banana";
-var letra = "a";
-var g =  prueba(palabra, letra);
